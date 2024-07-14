d9f005afacdbf3da7fed77c36975bcea06ca3f81
giho <giho7427@daum.net>
2024-07-14 21:29:07 +0900

    refactor: NotFound 페이지 간단 수정

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,3 +1,17 @@
+import styled from 'styled-components';
+
 export default function NotFoundPage() {
-  return <>페이지를 찾을 수 없습니다.</>;
+  return <BackGround>페이지를 찾을 수 없습니다.</BackGround>;
 }
+
+const BackGround = styled.div`
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 24px;
+  font-weight: bold;
+  color: var(--grey-grey100);
+`;
