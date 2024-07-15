a61d6c76d2ca3bf62a9e620f282c44bd7909f794
giho <giho7427@daum.net>
2024-07-16 03:05:48 +0900

    refactor: 필요없는 코드 삭제

--- src/pages/Main/MainPage.hook.tsx
+++ src/pages/Main/MainPage.hook.tsx
@@ -1,19 +1,19 @@
-// hooks
-import useRenewTokens from '@/apis/hooks/useRenewTokens';
-import { useApplicationAuthTokenStore } from '@/store/useAuthStore';
-import { useEffect } from 'react';
+// // hooks
+// import useRenewTokens from '@/apis/hooks/useRenewTokens';
+// import { useApplicationAuthTokenStore } from '@/store/useAuthStore';
+// import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function useMainPage() {
   const navigate = useNavigate();
-  const { setAccessToken } = useApplicationAuthTokenStore();
-  const { data, isLoading, isSuccess, isError } = useRenewTokens();
+  // const { setAccessToken } = useApplicationAuthTokenStore();
+  // const { data, isLoading, isSuccess, isError } = useRenewTokens();
 
-  useEffect(() => {
-    if (isSuccess) {
-      setAccessToken(data.accessToken);
-    }
-  }, [data]);
+  // useEffect(() => {
+  //   if (isSuccess) {
+  //     setAccessToken(data.accessToken);
+  //   }
+  // }, [data]);
 
-  return { isLoading, isError, navigate };
+  return { navigate };
 }
