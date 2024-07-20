daa720ba9802b7512c1c77f7902d65866fa967ab
giho <giho7427@daum.net>
2024-07-20 23:04:35 +0900

    refactor: 웹팩 1차 최적화

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
 var Dotenv = require('dotenv-webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+var { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+var ProgressPlugin = require('progress-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: 'bundle.js', // 번들된 파일의 이름
+    path: path.resolve(__dirname, 'dist'), // 번들된 파일 저장 경로
+    publicPath: '/', // 번들 파일이 제공될 URL 경로
+    chunkFilename: '[name].bundle.js', // 동적 임포트된 청크 파일 이름
+    assetModuleFilename: 'images/[hash][ext][query]', // 자산 파일 이름 형식(이미지, 폰트 등)
+    clean: true, // 이전 빌드 결과물 삭제
   },
   module: {
     rules: [
@@ -19,45 +24,30 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
+          // 마지막 프리셋부터 거꾸로 실행
           presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript'],
-        },
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-        options: {
-          minimize: true,
         },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
       },
     ],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@/apis': path.resolve(__dirname, 'src/apis'),
-      '@/components': path.resolve(__dirname, 'src/components'),
-      '@/images': path.resolve(__dirname, 'src/images'),
-      '@/mocks': path.resolve(__dirname, 'src/mocks'),
-      '@/pages': path.resolve(__dirname, 'src/pages'),
-      '@/router': path.resolve(__dirname, 'src/router'),
-      '@/store': path.resolve(__dirname, 'src/store'),
-      '@/styles': path.resolve(__dirname, 'src/styles'),
-      '@/types': path.resolve(__dirname, 'src/types'),
-      '@/utils': path.resolve(__dirname, 'src/utils'),
-    },
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.json',
+      }),
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-    }),
-    new CleanWebpackPlugin.CleanWebpackPlugin(),
-    new Dotenv({
-      path: './.env.local', // .env.local 파일을 명시적으로 지정
     }),
     new CopyWebpackPlugin({
       patterns: [
@@ -66,14 +56,15 @@ module.exports = {
         { from: 'public/manifest.json', to: 'manifest.json' },
       ],
     }),
+    new Dotenv({
+      path: './.env.local',
+    }),
+    new BundleAnalyzerPlugin(),
+    new ProgressPlugin(),
   ],
   devServer: {
     hot: true,
     port: 3000,
-    open: true,
-    historyApiFallback: {
-      rewrites: [{ from: /./, to: '/index.html' }],
-    },
-    static: path.resolve(__dirname, 'dist'), // 추가된 부분
+    historyApiFallback: true,
   },
 };
