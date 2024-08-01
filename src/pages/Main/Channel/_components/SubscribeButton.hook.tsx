c3730b34689cac360174eda46492bdda9e63f575
giho <giho7427@daum.net>
2024-08-02 00:52:24 +0900

    fix: 자동배포 오류 고치기

--- src/pages/Main/Channel/_components/SubscribeButton.hook.tsx
+++ src/pages/Main/Channel/_components/SubscribeButton.hook.tsx
@@ -6,11 +6,11 @@ import { useUserStore } from '@/store/useUserStore';
 
 // components
 import SubscribeChannelModal from '@/components/Organisms/Modal/SubscribeChannelModal';
-import UnSubscribeChannelModal from '@/components/Organisms/Modal/UnSubscribeChannelModal';
 
 // types
 import { ModalType } from '@/types/enum';
 import { UseSubscribeButtonProps } from './SubscribeButton.type';
+import UnSubscribeChannelModal from '@/components/Organisms/Modal/UnsubscribeChannelModal';
 
 export default function useSubscribeButton({ channelId }: UseSubscribeButtonProps) {
   const [isSubscribed, setIsSubscribed] = useState(false);
