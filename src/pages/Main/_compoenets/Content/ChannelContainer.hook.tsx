230041c1ecaafd2f32c8733ed83d573943d78273
giho <giho7427@daum.net>
2024-08-11 23:48:59 +0900

    feat: musicBar 레이아웃 사이드 이펙트 수정

--- src/pages/Main/_compoenets/Content/ChannelContainer.hook.tsx
+++ src/pages/Main/_compoenets/Content/ChannelContainer.hook.tsx
@@ -1,11 +1,14 @@
 import useGetAllChannels from '@/apis/hooks/useGetAllChannels';
+import useMusicStore from '@/store/useMusicStore';
 
 export default function useChannelContainer() {
   const { data: channels, isLoading, isError } = useGetAllChannels();
+  const { music: currentMusic } = useMusicStore();
 
   return {
     channels,
     isLoading,
     isError,
+    currentMusic,
   };
 }
