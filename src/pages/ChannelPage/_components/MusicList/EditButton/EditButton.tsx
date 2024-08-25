22ccd5c593f09bf1e3666e0e9337e2f7b54ecc40
giho <giho7427@daum.net>
2024-08-25 16:03:42 +0900

    refactor: useAuthStore 정리

--- src/pages/ChannelPage/_components/MusicList/EditButton/EditButton.tsx
+++ src/pages/ChannelPage/_components/MusicList/EditButton/EditButton.tsx
@@ -2,7 +2,6 @@
 import styled from 'styled-components';
 
 // types
-
 import useEditButton from './EditButton.hook';
 import { EditButtonProps } from './EditButton.type';
 
--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,31 +1,11 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-interface ApplicationAuthTokenStore {
-  accessToken: string | null;
-  setAccessToken: (accessToken: string | undefined) => void;
-  removeAccessToken: () => void;
-}
-
 interface GoogleOAuthTokenStore {
   googleOAuthToken: string | null;
   setGoogleOAuthToken: (googleOAuthToken: string) => void;
   removeGoogleOAuthToken: () => void;
 }
-
-export const useApplicationAuthTokenStore = create(
-  persist<ApplicationAuthTokenStore>(
-    (set) => ({
-      accessToken: null,
-      setAccessToken: (accessToken: string | undefined) => set({ accessToken }),
-      removeAccessToken: () => set({ accessToken: null }),
-    }),
-    {
-      name: 'applicationAuthTokenStore',
-      getStorage: () => localStorage,
-    },
-  ),
-);
 
 export const useGoogleOAuthTokenStore = create(
   persist<GoogleOAuthTokenStore>(
