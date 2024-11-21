010cfb46ef43a3964da096c0a8612ee6af0896da
giho <giho7427@daum.net>
2024-11-21 15:30:26 +0900

    feat: 트리거 UI 생성

--- src/pages/MainPage/_compoenets/Content/ChannelContainer/ChannelContainer.hook.tsx
+++ src/pages/MainPage/_compoenets/Content/ChannelContainer/ChannelContainer.hook.tsx
@@ -1,13 +1,36 @@
 import useGetChannelByPageParam from '@/apis/hooks/useGetChannelsByPageParam';
 import useMusicStore from '@/store/useMusicStore';
+import { useEffect, useRef } from 'react';
 
 export default function useChannelContainer() {
   const { data: channels, fetchNextPage } = useGetChannelByPageParam();
   const { music: currentMusic } = useMusicStore();
+  const InfinifeScrollTriggerRef = useRef<HTMLDivElement | null>(null);
+
+  const infiniteScroll = async () => {
+    if (InfinifeScrollTriggerRef.current) {
+      const io = new IntersectionObserver(
+        (entries) => {
+          entries.forEach(async (entry) => {
+            if (entry.isIntersecting && InfinifeScrollTriggerRef.current) {
+              fetchNextPage();
+            }
+          });
+        },
+        { threshold: 1 },
+      );
+
+      io.observe(InfinifeScrollTriggerRef.current);
+    }
+  };
+
+  useEffect(() => {
+    infiniteScroll();
+  }, []);
 
   return {
     channels,
-    fetchNextPage,
     currentMusic,
+    InfinifeScrollTriggerRef,
   };
 }
--- src/pages/MainPage/_compoenets/Content/ChannelContainer/ChannelContainer.tsx
+++ src/pages/MainPage/_compoenets/Content/ChannelContainer/ChannelContainer.tsx
@@ -7,6 +7,7 @@ import useChannelContainer from './ChannelContainer.hook';
 
 // components
 import MainTitle from '@/components/Atoms/Text/MainTitle';
+import { Spinner } from '@/components/Atoms/Spinner/ComponentSpinner';
 
 const Channel = lazy(() => import('./Channel/Channel'));
 
@@ -15,7 +16,7 @@ import { Music } from '@/types/music';
 
 export default function ChannelContainer() {
   // logics
-  const { channels, currentMusic, fetchNextPage } = useChannelContainer();
+  const { channels, currentMusic, InfinifeScrollTriggerRef } = useChannelContainer();
 
   // view
   return (
@@ -26,14 +27,9 @@ export default function ChannelContainer() {
         {channels?.pages.map((page) => page.channels?.map((channel) => <Channel key={channel.id} channel={channel} />))}
       </Container>
 
-      <button
-        onClick={() => {
-          fetchNextPage();
-          console.log('fetchNextPage :', channels);
-        }}
-      >
-        fetchNextPage
-      </button>
+      <InfiniteScrollTrigger ref={InfinifeScrollTriggerRef}>
+        <Spinner />
+      </InfiniteScrollTrigger>
     </Wrapper>
   );
 }
@@ -73,4 +69,12 @@ const Container = styled.ul<{
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, 1fr);
   }
+`;
+
+const InfiniteScrollTrigger = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
