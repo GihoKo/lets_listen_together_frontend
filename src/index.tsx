537ff32941e7ca8fcad5a78241fba367ff42073a
giho <giho7427@daum.net>
2024-12-12 11:51:51 +0900

    feat: PersistQueryClient를 이용해 특정 쿼리 로컬 스토리지에 저장하기

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Query, QueryClient } from '@tanstack/react-query';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,6 +16,19 @@ const queryClient = new QueryClient({
   },
 });
 
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+  key: 'react-query-persist-local-storage',
+  throttleTime: 1000,
+  serialize: (value) => JSON.stringify(value),
+  deserialize: (value) => JSON.parse(value),
+});
+
+// 특정 쿼리 키의 값만 로컬스토리지에 저장
+const dehydrateOptions = {
+  shouldDehydrateQuery: (query: Query) => query.queryKey.includes('musicList'),
+};
+
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
@@ -22,10 +36,10 @@ if (rootElement) {
   root.render(
     // <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+      <PersistQueryClientProvider client={queryClient} persistOptions={{ persister, dehydrateOptions }}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} position='left' />
-      </QueryClientProvider>
+      </PersistQueryClientProvider>
     </BrowserRouter>,
     // </React.StrictMode>,
   );
